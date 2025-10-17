e23bf7ef11ccfce30541b4452b2ce29d7a328a93
Jakub Jablonski <kubex564@gmail.com>
2025-10-17 12:34:19 +0200

    finished z3

--- lab2/z4/script.js
+++ lab2/z4/script.js
@@ -2,49 +2,89 @@ const blueSquare = document.getElementById(`blue`);
 const redSquare = document.getElementById(`red`);
 const yellowSquare = document.getElementById(`yellow`);
 const console = document.getElementById(`console`);
+const score = document.getElementById(`score`);
+const propagationButton = document.getElementById(`togglePropagation`);
+const resetButton = document.getElementById(`reset`);
+const orderButton = document.getElementById(`order`);
 
-let score = 0;
+let scoreValue = 0;
+let propagationEnabled = true;
+let isCapture = false;
 
-function blueEvent()
-{
-    console.innerHTML += "Nacisnąłeś niebieski o wartości 1<br>";
-    score += 1;
+function updateScore(){
+    score.innerHTML = "Score: " + scoreValue;
 }
 
-function redEvent()
+function blueEvent(event)
 {
-    console.innerHTML += "Nacisnąłeś czerwony o wartości 2<br>";
-    score += 2;
+    if(!propagationEnabled){
+        event.stopPropagation();
+    }
+    console.innerHTML += "Nacisnąłeś niebieski o wartości 1<br>";
+    scoreValue += 1;
+    updateScore();
+}
 
-    if(score > 30){
+function redEvent(event)
+{
+    if(!propagationEnabled){
+        event.stopPropagation();
+    }
+    console.innerHTML += "Nacisnąłeś czerwony o wartości 2<br>";
+    scoreValue += 2;
+    updateScore();
+    if(scoreValue > 30){
         redSquare.removeEventListener('click', redEvent);
     }
 }
 
-function yellowEvent()
+function yellowEvent(event)
 {
+    if(!propagationEnabled){
+        event.stopPropagation();
+    }
     console.innerHTML += "Nacisnąłeś żółty o wartości 5<br>";
-    score += 5;
-
-    if(score > 50){
+    scoreValue += 5;
+    updateScore();
+    if(scoreValue > 50){
         yellowSquare.removeEventListener('click', yellowEvent);
     }
 }
 
-blueSquare.addEventListener('click', blueEvent);
-
-redSquare.addEventListener('click', redEvent);
-
-yellowSquare.addEventListener('click', yellowEvent);
-
-
 function disableAll(){
-    blueSquare.removeEventListener('click', blueEvent);
-    redSquare.removeEventListener('click', redEvent);
-    yellowSquare.removeEventListener('click', yellowEvent);
+    blueSquare.removeEventListener('click', blueEvent, isCapture);
+    redSquare.removeEventListener('click', redEvent, isCapture);
+    yellowSquare.removeEventListener('click', yellowEvent, isCapture);
 }
 
 
-if(score >= 10){
+function activateAll(){
+    blueSquare.addEventListener('click', blueEvent, isCapture);
+    redSquare.addEventListener('click', redEvent, isCapture);
+    yellowSquare.addEventListener('click', yellowEvent, isCapture);
+}
+
+activateAll();
+
+propagationButton.addEventListener('click', (event)=>{
+    propagationEnabled = !propagationEnabled;
+    propagationButton.innerHTML = propagationEnabled ? "Stop Propagation" : "Start Propagation";
+});
+
+orderButton.addEventListener('click', ()=>{
     disableAll();
-}
+    isCapture = !isCapture;
+    orderButton.innerHTML = isCapture ? "Order: Capture" : "Order: Bubble";
+    activateAll(propagationEnabled);
+});
+    
+
+resetButton.addEventListener('click', ()=>{
+    scoreValue = 0;
+    updateScore();
+    console.innerHTML = "";
+    disableAll();
+    blueSquare.addEventListener('click', blueEvent);
+    redSquare.addEventListener('click', redEvent);
+    yellowSquare.addEventListener('click', yellowEvent);
+});
